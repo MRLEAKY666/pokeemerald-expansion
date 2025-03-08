dfaa7a619f02399d6e201bb5208d420bc157f2ad
MRLEAKY666 <ichabodzero@gmail.com>
2025-03-07 21:51:08 -0600

    enabled dexnav
    used some new flags/vars

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -269,8 +269,8 @@
 #define VAR_MOLTRES_INDEX                               0x40F9 // PREVIOUSLY Unused Var
 #define VAR_BEAST_SPECIES                                0x40FA // PREVIOUSLY Unused Var
 #define VAR_LATI_INDEX                                0x40FB // PREVIOUSLY Unused Var
-#define VAR_UNUSED_0x40FC                                0x40FC // Unused Var
-#define VAR_UNUSED_0x40FD                                0x40FD // Unused Var
+#define VAR_DEXNAV_SPECIES                                0x40FC // previously Unused Var
+#define VAR_DEXNAV_STEP_COUNTER                                0x40FD // previously Unused Var
 #define VAR_UNUSED_0x40FE                                0x40FE // Unused Var
 #define VAR_UNUSED_0x40FF                                0x40FF // Unused Var
 
